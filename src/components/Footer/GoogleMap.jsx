8d8c84cedca612d0e4c984f303606bbe28deb6a1
井手翔陽 <shouhi605@gmail.com>
2022-07-25 14:00:29 +0900

    responsive(仮)

--- src/components/Footer/GoogleMap.jsx
+++ src/components/Footer/GoogleMap.jsx
@@ -1,9 +1,25 @@
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { useEffect, useState } from 'react';
 
 const Map = () => {
+  const [width, setWidth] = useState(400);
+  const breakpoint = 900;
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
   const mapStyles = {
     height: '400px',
     width: '800px',
+  };
+
+  const responsiveStyles = {
+    height: '300px',
+    width: '350px',
   };
 
   const defaultCenter = {
@@ -27,7 +43,7 @@ const Map = () => {
     >
       <div style={{ marginTop: '50px' }}>
         <GoogleMap
-          mapContainerStyle={mapStyles}
+          mapContainerStyle={width < breakpoint ? responsiveStyles : mapStyles}
           zoom={13}
           center={defaultCenter}
         >
