f1a78905580d637720ef1fd50dd06feff1494b36
井手翔陽 <shouhi605@gmail.com>
2022-07-26 12:11:35 +0900

    fix about section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -12,18 +12,8 @@ const AboutUs = () => (
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
         <p className="p__opensans">2022年5月に神戸三宮に「エンジニアが集まるバー」としてオープンいたしました</p>
-        <button type="button" className="custom__button">Know More</button>
-      </div>
-
-      <div className="app__aboutus-content_knife flex__center">
-        {/* <img src={images.knife} alt="about_knife" /> */}
-      </div>
-
-      <div className="app__aboutus-content_history">
-        <h1 className="headtext__cormorant">Our History</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">5月</p>
-        <button type="button" className="custom__button">Know More</button>
+        <p className="p__opensans">エンジニア同士だからこそ繰り広げられるこの空間をどうぞお楽しみください</p>
+        {/* <button type="button" className="custom__button">Know More</button> */}
       </div>
     </div>
   </div>
