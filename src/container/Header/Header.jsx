99f7d0054808d148da61e96f499046f74ca4683f
井手翔陽 <shouhi605@gmail.com>
2022-07-26 12:11:19 +0900

    welcome画像をcssに変更

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,9 +8,12 @@ const Header = () => (
       <SubHeading title="エンジニアが集まるバー" />
       <h1 className="app__header-h1">Hack.BAR</h1>
       <p className="p__opensans" style={{ margin: '2rem 0' }}>神戸三宮駅から徒歩5分。「エンジニアが集まるバー」としてオープンしました</p>
-      <button type="button" className="custom__button">Explore Menu</button>
+      <button type="button" className="custom__button">
+        <a href="#menu">
+          Explore Menu
+        </a>
+      </button>
     </div>
-
     <div className="app__wrapper_img">
       <img src={images.welcome} alt="header_img" />
     </div>
