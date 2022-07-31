8db91619dcc7041831b2911a30e3aab58e4082a5
井手翔陽 <shouhi605@gmail.com>
2022-08-01 08:36:50 +0900

    add new img

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,12 +1,14 @@
 import bg from '../assets/bg.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
+import gallery01 from '../assets/gallery01.jpg';
+import gallery02 from '../assets/gallery02.jpg';
+import gallery03 from '../assets/gallery03.jpg';
+import gallery04 from '../assets/gallery04.jpg';
+import gallery05 from '../assets/gallery05.jpg';
+import gallery06 from '../assets/gallery06.jpg';
 import menu from '../assets/menu.png';
 import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
-import welcome from '../assets/welcome.png';
+import welcome from '../assets/welcome.jpg';
 import quote from '../assets/quote.png';
 
 const images = {
@@ -15,6 +17,8 @@ const images = {
   gallery02,
   gallery03,
   gallery04,
+  gallery05,
+  gallery06,
   menu,
   overlaybg,
   spoon,
