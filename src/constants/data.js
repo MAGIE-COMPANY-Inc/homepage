3623aa556980638b8c6551573841c301d3d8fc41
hinata <chouxingye@gmail.com>
2022-08-10 15:20:25 +0900

    change menu

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,45 +1,45 @@
 export const cocktails = [
   {
-    title: 'React.js',
-    price: '¥700',
-    tags: 'BLUE CURACAO  | LEJAY PEACH  | soda ',
+    title: "JavaScript",
+    price: "¥700",
+    tags: "Dry Gin | Orange juice | Ginger Ale",
   },
   {
-    title: 'Vue.js',
-    price: '¥700',
-    tags: 'BLUE CURACAO | LEJAY PEACH  | Orange juice | soda',
+    title: "React.js",
+    price: "¥700",
+    tags: "Tequila | BLUE CURACAO | Lemon juice | Tonic Water",
   },
   {
-    title: 'JavaScript',
-    price: '¥700',
-    tags: 'LEJAY PEACH  | DITA LYCHEE | Orange juice | soda',
+    title: "Vue.js",
+    price: "¥700",
+    tags: "Melon Liqueur | Vodka | Pineapple Juice",
   },
   {
-    title: 'Rust',
-    price: '¥700',
-    tags: 'CAPPARI | Coke ',
+    title: "Python",
+    price: "¥700",
+    tags: " DITA LYCHEE | Grapefruit Juice | Tonic Water",
   },
   {
-    title: 'Vim',
-    price: '¥700',
-    tags: 'BLUE CURACAO | Coke | soda | Lime',
+    title: "Golang",
+    price: "¥700",
+    tags: "LEJAY PEACH | BLUE CURACAO | Grapefruit Juice | Tonic Water",
   },
 ];
 
 export const whiskys = [
   {
     title: "Jack Daniel's",
-    price: '¥700',
-    tags: '一番人気のジャックダニエル ブラック。テネシーウイスキーとしてバーボンとは別でランクされている銘柄です',
+    price: "¥700",
+    tags: "一番人気のジャックダニエル ブラック。テネシーウイスキーとしてバーボンとは別でランクされている銘柄です",
   },
   {
-    title: 'Chivas Regal 12years',
-    price: '¥800',
-    tags: '様々な個性の異なるモルトウイスキーとグレーンウイスキーの芸術的なブレンド（アート・オブ・ブレンディング）によって創り出される、リッチでスムースな味わいのウイスキーです。',
+    title: "Chivas Regal 12years",
+    price: "¥800",
+    tags: "様々な個性の異なるモルトウイスキーとグレーンウイスキーの芸術的なブレンド（アート・オブ・ブレンディング）によって創り出される、リッチでスムースな味わいのウイスキーです。",
   },
   {
-    title: 'Chivas Regal MIZUNARA 12years',
-    price: '¥900',
-    tags: '日本原産の希少なミズナラ樽でマリッジして仕上げるこのウイスキーは、スコットランドから日本への心を込めた贈りものです。まろやかさと、ハチミツのような濃厚な甘みが感じられます。',
+    title: "Chivas Regal MIZUNARA 12years",
+    price: "¥900",
+    tags: "日本原産の希少なミズナラ樽でマリッジして仕上げるこのウイスキーは、スコットランドから日本への心を込めた贈りものです。まろやかさと、ハチミツのような濃厚な甘みが感じられます。",
   },
 ];
