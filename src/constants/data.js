1741314cdcb16132e172d58b14d9be305e9f1021
井手翔陽 <shouhi605@gmail.com>
2022-08-22 18:59:15 +0900

    ci/cd

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,45 +1,45 @@
 export const cocktails = [
   {
-    title: "JavaScript",
-    price: "¥700",
-    tags: "Dry Gin | Orange juice | Ginger Ale",
+    title: 'JavaScript',
+    price: '¥700',
+    tags: 'Dry Gin | Orange juice | Ginger Ale',
   },
   {
-    title: "React.js",
-    price: "¥700",
-    tags: "Tequila | BLUE CURACAO | Lemon juice | Tonic Water",
+    title: 'React.js',
+    price: '¥700',
+    tags: 'Tequila | BLUE CURACAO | Lemon juice | Tonic Water',
   },
   {
-    title: "Vue.js",
-    price: "¥700",
-    tags: "Melon Liqueur | Vodka | Pineapple Juice",
+    title: 'Vue.js',
+    price: '¥700',
+    tags: 'Melon Liqueur | Vodka | Pineapple Juice',
   },
   {
-    title: "Python",
-    price: "¥700",
-    tags: " DITA LYCHEE | Grapefruit Juice | Tonic Water",
+    title: 'Python',
+    price: '¥700',
+    tags: ' DITA LYCHEE | Grapefruit Juice | Tonic Water',
   },
   {
-    title: "Rust",
-    price: "¥700",
-    tags: "CAPPARI | Coke",
+    title: 'Rust',
+    price: '¥700',
+    tags: 'CAPPARI | Coke',
   },
 ];
 
 export const whiskys = [
   {
     title: "Jack Daniel's",
-    price: "¥700",
-    tags: "一番人気のジャックダニエル ブラック。テネシーウイスキーとしてバーボンとは別でランクされている銘柄です",
+    price: '¥700',
+    tags: '一番人気のジャックダニエル ブラック。テネシーウイスキーとしてバーボンとは別でランクされている銘柄です',
   },
   {
-    title: "Chivas Regal 12years",
-    price: "¥800",
-    tags: "様々な個性の異なるモルトウイスキーとグレーンウイスキーの芸術的なブレンド（アート・オブ・ブレンディング）によって創り出される、リッチでスムースな味わいのウイスキーです。",
+    title: 'Chivas Regal 12years',
+    price: '¥800',
+    tags: '様々な個性の異なるモルトウイスキーとグレーンウイスキーの芸術的なブレンド（アート・オブ・ブレンディング）によって創り出される、リッチでスムースな味わいのウイスキーです。',
   },
   {
-    title: "Chivas Regal MIZUNARA 12years",
-    price: "¥900",
-    tags: "日本原産の希少なミズナラ樽でマリッジして仕上げるこのウイスキーは、スコットランドから日本への心を込めた贈りものです。まろやかさと、ハチミツのような濃厚な甘みが感じられます。",
+    title: 'Chivas Regal MIZUNARA 12years',
+    price: '¥900',
+    tags: '日本原産の希少なミズナラ樽でマリッジして仕上げるこのウイスキーは、スコットランドから日本への心を込めた贈りものです。まろやかさと、ハチミツのような濃厚な甘みが感じられます。',
   },
 ];
