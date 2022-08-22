928fbf703dfc6ca5d761e1db7619cd8a48e44017
井手翔陽 <shouhi605@gmail.com>
2022-08-22 19:19:04 +0900

    rm comment

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -13,7 +13,6 @@ const AboutUs = () => (
         <img src={images.spoon} alt="about_section" className="spoon__img" />
         <p className="p__opensans">2022年5月に神戸三宮に「エンジニアが集まるバー」としてオープンいたしました</p>
         <p className="p__opensans">エンジニア同士だからこそ繰り広げられるこの空間をどうぞお楽しみください</p>
-        {/* <button type="button" className="custom__button">Know More</button> */}
       </div>
     </div>
   </div>
