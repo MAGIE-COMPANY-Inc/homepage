d3f1732b15247c4d1b182a785f3c4e1be44d16ff
井手翔陽 <shouhi605@gmail.com>
2022-08-24 23:34:00 +0900

    add tweetLimit

--- src/components/Footer/Twitter.jsx
+++ src/components/Footer/Twitter.jsx
@@ -6,7 +6,7 @@ const Twitter = () => (
   <div className="app_twitter">
     <div className="app_twitter-heading">
       <SubHeading title="Twitter" />
-      <TwitterTimelineEmbed sourceType="profile" screenName="EngineerBarKobe" options={{ height: 400 }} />
+      <TwitterTimelineEmbed sourceType="profile" screenName="EngineerBarKobe" options={{ height: 400 }} tweetLimit="5" />
       <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem', marginBottom: '3rem' }}>ツイッターでの発信を主に行っています。是非フォローして新着情報をお楽しみにください</p>
       <TwitterHashtagButton tag="ハックバー" />
       <TwitterFollowButton screenName="EngineerBarKobe" />
