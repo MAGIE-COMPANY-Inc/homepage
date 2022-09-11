48265750d49697a59a898b76d362c91a6be84ee9
井手翔陽 <shouhi605@gmail.com>
2022-09-11 22:44:36 +0900

    Menu追加

--- src/constants/data.js
+++ src/constants/data.js
@@ -26,6 +26,49 @@ export const cocktails = [
   },
 ];
 
+export const cocktails2 = [
+  {
+    title: 'C',
+    price: '¥700',
+    tags: 'DITA LYCHEE | Grapefruit Juice | tonic water',
+  },
+  {
+    title: 'Go',
+    price: '¥700',
+    tags: 'Peach Liqueur | BLUE CURACAO | Grapefruit Juice | Tonic Water',
+  },
+  {
+    title: 'Laravel',
+    price: '¥700',
+    tags: 'Tequila | Cassis Liqueur | Lemon juice | Ginger Ale',
+  },
+  {
+    title: 'Swift',
+    price: '¥700',
+    tags: 'Passoa | Grapefruit Juice | Tonic Water',
+  },
+  {
+    title: 'Ruby',
+    price: '¥700',
+    tags: 'Raspberries | Lime juice | Soda',
+  },
+  {
+    title: 'C#',
+    price: '¥700',
+    tags: 'Violet Liqueur | Dry Gin | Lemon juice | Soda',
+  },
+  {
+    title: 'VanillaJS',
+    price: '¥700',
+    tags: 'Baileys Irish Cream | Kahlua and Milk | RAM',
+  },
+  {
+    title: 'Hackers Highball',
+    price: '¥700',
+    tags: '東京にあるHackersBarの中尾様から公認で頂いたためメニューの公表は致しかねます。',
+  },
+];
+
 export const whiskys = [
   {
     title: "Jack Daniel's",
@@ -41,5 +84,28 @@ export const whiskys = [
     title: 'Chivas Regal MIZUNARA 12years',
     price: '¥900',
     tags: '日本原産の希少なミズナラ樽でマリッジして仕上げるこのウイスキーは、スコットランドから日本への心を込めた贈りものです。まろやかさと、ハチミツのような濃厚な甘みが感じられます。',
+  },
+];
+
+export const whiskys2 = [
+  {
+    title: 'TALISKER',
+    price: '¥1000',
+    tags: 'アイランズ系のシングルモルトでスモーキーかつ、コショウのようなスパイシーさと潮気、そして分厚く、重厚感のある甘い余韻を持つ人気の高いウイスキーです。',
+  },
+  {
+    title: 'Ballantines',
+    price: '¥1000',
+    tags: '「ザ・スコッチ」と名高いウィスキーで、華やかな香りと甘み、アイラのスモーキーさやピート香が特徴。バランスのとれた味わいとまろやかな香りは、ウィスキー初心者でも飲みやすい味わいです。',
+  },
+  {
+    title: 'BOWMORE',
+    price: '¥1000',
+    tags: 'スモーキーさと芳醇なピートの香り。そして、味わいの奥に感じ取れるゴージャスな甘さにあります。クセが非常に強いものの、この甘さからボウモアは「アイラモルトの女王」と評価されています。',
+  },
+  {
+    title: 'Ardbeq',
+    price: '¥1000',
+    tags: '独特のスモーキーさと、磯を思わせる香り(ピート香)は、好き嫌いがはっきり分かれる味わい。アードベッグはアイラ・モルトの中でも飛びぬけて癖の強く、スモーキーなのが特徴です。',
   },
 ];
--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -1,42 +1,64 @@
-import { whiskys, cocktails } from '../../constants/data';
+import { useState } from 'react';
+import { whiskys, whiskys2, cocktails, cocktails2 } from '../../constants/data';
 import { MenuItem } from '../../components/Menuitem/MenuItem';
 import images from '../../constants/images';
 import './Menu.css';
 
-const Menu = () => (
-  <div className="app__specialMenu flex__center section__padding" id="menu">
-    <div className="app__specialMenu-title">
-      <h1 className="headtext__cormorant">Drink Menu</h1>
-    </div>
+const Menu = () => {
+  const [show, setShow] = useState(false);
 
-    <div className="app__specialMenu-menu">
-      <div className="app__specialMenu-menu_wine  flex__center">
-        <p className="app__specialMenu-menu_heading">Whisky</p>
-        <div className="app__specialMenu_menu_items">
-          {whiskys.map((whisky, index) => (
-            <MenuItem key={whisky.title + index} title={whisky.title} price={whisky.price} tags={whisky.tags} />
-          ))}
+  const showViewMore = () => {
+    setShow(!show);
+  };
+
+  return (
+
+    <div className="app__specialMenu flex__center section__padding" id="menu">
+      <div className="app__specialMenu-title">
+        <h1 className="headtext__cormorant">Drink Menu</h1>
+      </div>
+
+      <div className="app__specialMenu-menu">
+        <div className="app__specialMenu-menu_wine  flex__center">
+          <p className="app__specialMenu-menu_heading">Whisky</p>
+          <div className="app__specialMenu_menu_items">
+            {whiskys.map((whisky, index) => (
+              <MenuItem key={whisky.title + index} title={whisky.title} price={whisky.price} tags={whisky.tags} />
+            ))}
+          </div>
+          <div className={`app__specialMenu_menu_items ${show ? 'txt-show' : 'txt-hide'}`}>
+            {whiskys2.map((whisky, index) => (
+              <MenuItem key={whisky.title + index} title={whisky.title} price={whisky.price} tags={whisky.tags} />
+            ))}
+          </div>
+        </div>
+
+        <div className="app__specialMenu-menu_img">
+          <img src={images.menu} alt="メニュー画像" />
+        </div>
+
+        <div className="app__specialMenu-menu_cocktails  flex__center">
+          <p className="app__specialMenu-menu_heading">Cocktails</p>
+          <div className="app__specialMenu_menu_items">
+            {cocktails.map((cocktail, index) => (
+              <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
+            ))}
+          </div>
+          <div className={`app__specialMenu_menu_items ${show ? 'txt-show' : 'txt-hide'}`}>
+            {cocktails2.map((whisky, index) => (
+              <MenuItem key={whisky.title + index} title={whisky.title} price={whisky.price} tags={whisky.tags} />
+            ))}
+          </div>
         </div>
       </div>
 
-      <div className="app__specialMenu-menu_img">
-        <img src={images.menu} alt="メニュー画像" />
-      </div>
-
-      <div className="app__specialMenu-menu_cocktails  flex__center">
-        <p className="app__specialMenu-menu_heading">Cocktails</p>
-        <div className="app__specialMenu_menu_items">
-          {cocktails.map((cocktail, index) => (
-            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
-          ))}
-        </div>
+      <div style={{ marginTop: 15 }}>
+        <button type="button" className="custom__button" onClick={showViewMore}>
+          {show ? 'View less' : 'View more'}
+        </button>
       </div>
     </div>
-
-    <div style={{ marginTop: 15 }}>
-      <button type="button" className="custom__button">View More</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Menu;
