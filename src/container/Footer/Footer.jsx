6adba287fa90f4e012b12ac898da0128166d759c
井手翔陽 <shouhi605@gmail.com>
2022-09-11 22:57:54 +0900

    Footerまわり修正

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,57 +1,11 @@
-import { FiTwitter, FiInstagram, FiLinkedin } from 'react-icons/fi';
 import { FooterOverlay } from '../../components';
 import Twitter from '../../components/Footer/Twitter';
-import images from '../../constants/images';
 import './Footer.css';
 
 const Footer = () => (
   <div className="app__footer section__padding">
     <FooterOverlay />
     <Twitter />
-    <div className="app__footer-links">
-      <div className="app__footer-links_contact">
-        <h1 className="app__footer-headtext" id="contact">Contact Us</h1>
-        <p className="p__opensans">〒650-0001 兵庫県神戸市中央区加納町４丁目７−２６ 藤嶋ビル 地下一階</p>
-        <p className="p__opensans">090-9253-2515</p>
-      </div>
-
-      <div className="app__footer-links_logo">
-        <p className="p__opensans">&quot;世界中のエンジニアとの交流も支援しています&quot;</p>
-        <img src={images.spoon} className="footer_section" style={{ marginTop: 15 }} />
-        <div className="app__footer-links_icons">
-          <a
-            href="https://twitter.com/EngineerBarKobe"
-            style={{ color: 'white' }}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FiTwitter />
-          </a>
-          <a
-            href="https://www.instagram.com/kobe.engineer_bar/"
-            style={{ color: 'white' }}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FiInstagram />
-          </a>
-          <a
-            href="https://www.linkedin.com/company/hack-bar/"
-            style={{ color: 'white' }}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FiLinkedin />
-          </a>
-        </div>
-      </div>
-
-      <div className="app__footer-links_work">
-        <h1 className="app__footer-headtext">Opening Hours</h1>
-        <p className="p__opensans">Friday-Saturday:</p>
-        <p className="p__opensans">18:00 - 23:00 </p>
-      </div>
-    </div>
 
     <div className="footer__copyright">
       <p className="p__opensans">2022 Hack.BAR. All Rights reserved.</p>
