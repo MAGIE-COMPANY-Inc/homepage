5c6c2a458095e9ec571e850cf8672eb9b1b49ee9
shouhi <shouhi605@gmail.com>
2022-11-30 18:35:15 +0900

    :rocket: 12月カレンダー追記

--- src/container/Calender/Calender.jsx
+++ src/container/Calender/Calender.jsx
@@ -1,7 +1,7 @@
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import jaLocale from '@fullcalendar/core/locales/ja';
-import { events } from './eventData';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import { events } from "./eventData";
 
 const Calender = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
@@ -21,16 +21,6 @@ const Calender = () => (
         events={events}
       />
     </div>
-    <button type="button" className="custom__button">
-      <a
-        href="https://hackbar.jp/blog/hack-bar%e9%b3%a5%e5%8f%96%e9%96%8b%e5%82%ac%e6%b1%ba%e5%ae%9a%e3%81%9d%e3%81%ae%e6%ad%a3%e4%bd%93%e3%81%a8%e3%81%af/"
-        style={{ color: 'black' }}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        ※Hack.BAR in 鳥取についてはこちら！
-      </a>
-    </button>
   </div>
 );
 
