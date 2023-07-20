f94a4701166fd59aca772c9bba6fff6794ea8b02
ideshouhi <shouhi605@gmail.com>
2023-07-21 07:51:51 +0900

    add ga

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import { AboutUs, FindUs, Calender, Footer, Gallery, Header, Menu, Staff } from './container';
-import { Navbar } from './components';
-import './App.css';
-import { I18nextProvider } from 'react-i18next'
-import i18n from './i18n/config'
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
+import {
+  AboutUs,
+  FindUs,
+  Calender,
+  Footer,
+  Gallery,
+  Header,
+  Menu,
+  Staff,
+  // TestCalender,
+} from "./container";
+import { Navbar } from "./components";
+import "./App.css";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n/config";
 
-const App = () => (
-  <I18nextProvider i18n={i18n}>
-  <div className="container">
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <Menu />
-    <Staff />
-    <Gallery />
-    <FindUs />
-    <Calender />
-    <Footer />
-  </div>
-  </I18nextProvider>
-);
+const App = () => {
+  useEffect(() => {
+    // Google AnalyticsのトラッキングIDを設定します
+    ReactGA.initialize("UA-164898966-1");
+    // ページビューを送信します
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <div className="container">
+        <Navbar />
+        <Header />
+        <AboutUs />
+        <Menu />
+        <Staff />
+        <Gallery />
+        <FindUs />
+        <Calender />
+        {/* <TestCalender /> */}
+        <Footer />
+      </div>
+    </I18nextProvider>
+  );
+};
 
 export default App;
